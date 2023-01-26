dda97bb6c2068d54cf8497b5a7fbd925b10745a2
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-01-26 11:06:12 +0530

    Request/Response

--- file.js
+++ file.js
@@ -1,11 +1,17 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from Node JS server</h1></body>')
-    res.write('</html>');
-    res.end();
+    if(req.url === '/home') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome home');
+    } else if(req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome to About Us page');
+    } else if(req.url === '/node') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome to my Node Js project');
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Page Not Found');
+    }
 })
-server.listen(3000);
+server.listen(4000);
