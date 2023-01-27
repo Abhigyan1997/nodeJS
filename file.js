220d29330f54ad9fa392f757f2c852be5ee9a481
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-01-27 11:29:38 +0530

    Response 302

--- file.js
+++ file.js
@@ -1,17 +1,43 @@
 const http=require('http');
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
-    if(req.url === '/home') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Welcome home');
-    } else if(req.url === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Welcome to About Us page');
-    } else if(req.url === '/node') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Welcome to my Node Js project');
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Page Not Found');
+    const url=req.url;
+    const method=req.method;
+    if(url==='/') {
+        fs.readFile("message.text",{encoding:"utf-8"},(err,data)=>{
+            if (err)
+            console.log(err);
+        })
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body>${data}</body>`);
+        res.write('<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')  ;
+        res.write('</html>');
+       return res.end();
     }
-})
+    if(url==="/message" && method==="POST"){
+        const body=[];
+        req.on("data",(chunk)=>{
+            console.log(chunk);
+        body.push(chunk);
+            });
+            req.on('end',()=>{
+              const parsedBody=Buffer.concat(body).toString();
+              const message=parsedBody.split('=')[1];
+              fs.writeFileSync("message.text", message, (err)=>{
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();
+              });
+            })
+        
+        
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from Node JS server</h1></body>')
+    res.write('</html>');
+    res.end();
+});
 server.listen(4000);
