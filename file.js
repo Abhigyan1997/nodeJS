9af99131defb6d3be4c545be9376513caff55986
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-01-28 11:33:18 +0530

    Chat App Demo

--- file.js
+++ file.js
@@ -3,34 +3,51 @@ const fs=require('fs');
 const server=http.createServer((req,res)=>{
     const url=req.url;
     const method=req.method;
+    const body=[];
     if(url==='/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')  ;
-        res.write('</html>');
-       return res.end();
-    }
-    if(url==="/message" && method==="POST"){
-        const body=[];
+        
+        fs.readFile("message.text",{encoding:"utf-8"},(err,data)=>{
+            if (err){
+                console.log(err);
+            }
+            console.log(`data from file` +data);
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title></head>');
+            res.write(`<body>${data}</body>`)
+            res.write(`<body><form action="message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)  ;
+            res.write('</html>');
+            return res.end();
+        })
+       
+         }else if(url==="/message" && method==="POST"){
+        
         req.on('data',(chunk)=>{
             console.log(chunk);
         body.push(chunk);
             });
-            req.on('end',()=>{
+          return req.on('end',()=>{
               const parsedBody=Buffer.concat(body).toString();
               const message=parsedBody.split('=')[1];
-              fs.writeFileSync('message.text', 'DUMMY');
+              fs.writeFile('message.text', message,(err)=>{
+                if (err){
+                    console.log(err);
+                }
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();
+              });
             })
         
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
+       
     }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from Node JS server</h1></body>')
-    res.write('</html>');
-    res.end();
+    else{
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Hello from Node JS server</h1></body>')
+        res.write('</html>');
+        res.end();
+    }
+    
 });
-server.listen(2000);
+server.listen(3000);
