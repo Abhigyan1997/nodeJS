cd6be4a16d5584b466da84f386a751caae5067f6
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-01-31 11:47:59 +0530

    Add Product

--- file.js
+++ file.js
@@ -1,13 +1,19 @@
 
 const express=require('express');
+const bodyParser=require('body-parser')
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('I am Middleware');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="number" name="size"><button type="submit">Add Product</button></input></form>');
 });
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
     console.log('I am another Middleware');
     res.send('<h1>Hello from Express JS!</h1>')
 });
