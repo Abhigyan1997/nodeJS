1668bf567781302192d3a629d35fec97dd7796a3
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-01-31 14:53:02 +0530

    Status 404

--- file.js
+++ file.js
@@ -1,21 +1,21 @@
 
 const express=require('express');
-const bodyParser=require('body-parser')
+
+const bodyParser=require('body-parser');
+
+const adminRoute=require('./route/admin');
+const shopRoute=require('./route/shop');
 
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="number" name="size"><button type="submit">Add Product</button></input></form>');
+app.use(adminRoute);
+app.use(shopRoute);
+
+app.use((req , res , next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>');
 });
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/',(req,res,next)=>{
-    console.log('I am another Middleware');
-    res.send('<h1>Hello from Express JS!</h1>')
-});
+
 
 app.listen(5000);
