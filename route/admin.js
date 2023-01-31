7bf9f7abe6f883b8ccd818548503b9f7acc16c13
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-01-31 15:05:32 +0530

    Admin Route

---
+++ route/admin.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const route=express.Router();
+
+route.get('/add-product',(req,res,next)=>{
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><br><input type="number" name="size"><button type="submit">Add Product</button></input></form>');
+});
+route.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports=route; 
